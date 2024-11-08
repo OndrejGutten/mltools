e15ed2e16e5cac06e0437a0474ea16e04c7b8b51
Merelorn <ondrogutten@gmail.com>
2024-11-08 09:59:50 +0100

    restructured model and data

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="mltools",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where='./src'),
+    package_dir={'': 'src'},
     description="A collection of utility functions for handling data and intermediate operations in MLOps.",
     author="Ondrej Gutten",
     author_email="ondrogutten@gmail.com",
