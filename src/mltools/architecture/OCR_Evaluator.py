2463f69cb76ea9858b53e59d566b39175b44216b
Merelorn <ondrogutten@gmail.com>
2024-12-17 13:36:13 +0100

    OCR evaluator formatting fix

--- src/mltools/architecture/OCR_Evaluator.py
+++ src/mltools/architecture/OCR_Evaluator.py
@@ -34,7 +34,7 @@ class OCR_Evaluator():
         
     def predict(self, data: str | list[str] | pd.DataFrame) -> pd.DataFrame:
         formatted_data = self._input_to_df(data)
-        answer = pd.DataFrame(columns = ['method','known_fraction','known_length'])
+        answer = pd.DataFrame(columns = ['known_fraction','known_length'])
         for col in formatted_data.columns:
             intermediate_df = pd.DataFrame({'fraction':pd.Series(dtype=float), 'known_length':pd.Series(dtype=int), 'total_length':pd.Series(dtype=int)}, index = formatted_data.index)
             column_texts_without_nan = formatted_data[col].dropna()
@@ -42,9 +42,9 @@ class OCR_Evaluator():
             fraction_micro = intermediate_df.apply(lambda x: x['fraction'] * x['total_length'], axis=1).sum() / intermediate_df['total_length'].sum()
             #fraction_macro = intermediate_df['fraction'].mean()
             known_length = intermediate_df['known_length'].sum()
-            answer.loc[col] = [col, fraction_micro, known_length]
+            answer.loc[col] = [fraction_micro, known_length]
         
-        return answer.astype({'method': 'str', 'known_fraction': 'float64','known_length': 'int64'})
+        return answer.astype({'known_fraction': 'float64','known_length': 'int64'})
     
     def _calculate_metrics_for_single_text(self, text):
         words_df = pd.DataFrame(data = text.lower().split(),columns = ['words'])
